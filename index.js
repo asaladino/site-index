35f4dee0c5920a87ec0856959da8fb56945ec680
Adam Saladino <adam.saladino@ellucian.com>
2017-10-27 16:52:53 -0400

    Need to create options file.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let SitemapRepository = require('./Repository/SitemapRepository');
 let JsonUrlsRepository = require('./Repository/JsonUrlsRepository');
 
-let sitemapRepository = new SitemapRepository('https://www.sfccmo.edu/sitemap.xml');
+let sitemapRepository = new SitemapRepository('https://www.somesite.com/sitemap.xml');
 let urlsRepository = new JsonUrlsRepository('output/urls.json');
 
 sitemapRepository.findAllPages((progress) => {
