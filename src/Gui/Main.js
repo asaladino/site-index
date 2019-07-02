63008aae5c762cc989510754dc0a71db8cbbf077
Adam Saladino <adam@codingsimply.com>
2019-07-02 10:55:22 -0400

    Started building out more of the interface.

---
+++ src/Gui/Main.js
@@ -0,0 +1,47 @@
+// noinspection ES6CheckImport
+import PropTypes from 'prop-types';
+import gi from 'node-gtk';
+const Gtk = gi.require('Gtk', '3.0');
+const {Window, Label, Entry, Grid, Switch} = Gtk;
+
+const startGui = (args) => {
+    gi.startLoop();
+    Gtk.init();
+
+    const window = new Window({
+        title: 'Site Index',
+        window_position: Gtk.WindowPosition.CENTER
+    });
+    window.on('destroy', () => Gtk.mainQuit());
+    window.on('delete-event', () => false);
+
+    window.setWmclass('Site Index', 'Site Index');
+    window.setDefaultSize(200, 80);
+    const grid = new Grid();
+    grid.attach(new Label({label: `Domain`}), 0, 0, 1, 1);
+    grid.attach(new Entry({text: args.domain}), 1, 0, 1, 1);
+
+    grid.attach(new Label({label: `Output`}), 0, 1, 1, 1);
+    grid.attach(new Entry({text: args.output.filename}), 1, 1, 1, 1);
+
+    grid.attach(new Label({label: `Headers`}), 0, 2, 1, 1);
+    grid.attach(new Switch({active: args.headers}), 1, 2, 1, 1);
+
+    window.add(grid);
+
+    window.showAll();
+    Gtk.main();
+};
+
+Gtk.propTypes = {
+    WindowPosition: {
+        CENTER: PropTypes.object
+    }
+};
+Window.propTypes = {
+    setWmclass: PropTypes.func,
+    setDefaultSize: PropTypes.func,
+    showAll: PropTypes.func
+};
+
+export default startGui;
--- src/site-index.js
+++ src/site-index.js
@@ -5,15 +5,20 @@ import getUsage from "command-line-usage";
 import IndexController from "./Controller/IndexController.js";
 import menu from "./Model/Menu.js";
 import Args from "./Model/Args.js";
+import startGui from "./Gui/Main";
 
 let args = new Args(commandLineArgs(menu[1]["optionList"]));
 
-if (args.shouldShowHelp()) {
-    console.log(getUsage(menu));
+if(args.gui) {
+    startGui(args);
 } else {
-    if (args.verbose) {
-        console.log("\nStarting Site Index");
+    if (args.shouldShowHelp()) {
+        console.log(getUsage(menu));
+    } else {
+        if (args.verbose) {
+            console.log("\nStarting Site Index");
+        }
+        let indexController = new IndexController(args);
+        indexController.start().then();
     }
-    let indexController = new IndexController(args);
-    indexController.start().then();
 }
