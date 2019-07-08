56944db82da8626729c8391937fa3cc7b861319b
Adam Saladino <adam@codingsimply.com>
2019-07-08 09:54:34 -0400

    GUI will now scan and updated the progress.

--- src/Gui/Main.js
+++ src/Gui/Main.js
@@ -1,6 +1,7 @@
 // noinspection ES6CheckImport
 import PropTypes from 'prop-types';
 import gi from 'node-gtk';
+import IndexController from "../Controller/IndexController";
 
 const Gtk = gi.require('Gtk', '3.0');
 const {Window, Label, Entry, Grid, Switch, ProgressBar, Button} = Gtk;
@@ -28,13 +29,29 @@ const startGui = (args) => {
     grid.attach(new Label({label: `Headers`}), 0, 2, 1, 1);
     grid.attach(new Switch({active: args.headers}), 1, 2, 1, 1);
 
-    const progressBar = new ProgressBar({fraction: 0.4, 'show-text': true, text: 'Making progress.'});
+    const progressBar = new ProgressBar({fraction: 0.0});
     grid.attach(progressBar, 0, 3, 2, 1);
 
     const startButton = new Button({label: 'Start Index'});
     grid.attach(startButton, 1, 4, 1, 1);
+
+
     startButton.on('clicked', () => {
-        console.log(`Let's scan..`);
+        let indexController = new IndexController(args);
+        startButton.setSensitive(false);
+        startButton.setLabel("Indexing Site...");
+        indexController.start((event, progress) => {
+            if(event === 'progress') {
+                const percentComplete = progress.urls / (progress.urlsPool + progress.urls);
+                progressBar.setFraction(percentComplete);
+            }
+        }).then(() => {
+            startButton.setSensitive(true);
+            startButton.setLabel("Start Index");
+        }).catch(() => {
+            startButton.setSensitive(true);
+            startButton.setLabel("Start Index");
+        });
     });
 
     window.add(grid);
@@ -49,6 +66,13 @@ Gtk.propTypes = {
     },
     Grid: {
         attach: PropTypes.func
+    },
+    ProgressBar: {
+        setFraction: PropTypes.func
+    },
+    Button: {
+        setSensitive: PropTypes.func,
+        setLabel: PropTypes.func
     }
 };
 Window.propTypes = {
