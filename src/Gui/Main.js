5ced237ad537f5d9006a2029f1b0beccbdb2cbf4
Adam Saladino <adam@codingsimply.com>
2019-07-09 11:54:40 -0400

    Refined the layout.

--- src/Gui/Main.js
+++ src/Gui/Main.js
@@ -4,7 +4,7 @@ import gi from 'node-gtk';
 import IndexController from "../Controller/IndexController";
 
 const Gtk = gi.require('Gtk', '3.0');
-const {Window, Label, Entry, Grid, Switch, ProgressBar, Button} = Gtk;
+const {Window, Label, Entry, Grid, Switch, ProgressBar, Button, Box, Align} = Gtk;
 
 const startGui = (args) => {
     gi.startLoop();
@@ -18,22 +18,36 @@ const startGui = (args) => {
     window.on('delete-event', () => false);
 
     window.setWmclass('Site Index', 'Site Index');
-    window.setDefaultSize(200, 80);
-    const grid = new Grid();
-    grid.attach(new Label({label: `Domain`}), 0, 0, 1, 1);
-    grid.attach(new Entry({text: args.domain}), 1, 0, 1, 1);
+    window.setDefaultSize(400, 100);
+    const grid = new Grid({
+        'column-spacing': 5,
+        'row-spacing': 5,
+        'margin_left': 5,
+        'margin_right': 5,
+        'margin_top': 5,
+        'margin_bottom': 5
+    });
+    grid.attach(new Label({label: `Domain`, halign: 1}), 0, 0, 1, 1);
+    const domainEntry = new Entry({text: args.domain, hexpand: true});
+    grid.attach(domainEntry, 1, 0, 1, 1);
 
-    grid.attach(new Label({label: `Output`}), 0, 1, 1, 1);
-    grid.attach(new Entry({text: args.output.filename}), 1, 1, 1, 1);
+    grid.attach(new Label({label: `Output`, halign: 1}), 0, 1, 1, 1);
+    const outputEntry = new Entry({text: args.output.filename});
+    grid.attach(outputEntry, 1, 1, 1, 1);
 
-    grid.attach(new Label({label: `Headers`}), 0, 2, 1, 1);
-    grid.attach(new Switch({active: args.headers}), 1, 2, 1, 1);
+    grid.attach(new Label({label: `Headers`, halign: 1}), 0, 2, 1, 1);
+    const headersSwitch = new Switch({active: args.headers});
+    const headersSwitchBox = new Box();
+    headersSwitchBox.add(headersSwitch);
+    grid.attach(headersSwitchBox, 1, 2, 1, 1);
 
     const progressBar = new ProgressBar({fraction: 0.0});
     grid.attach(progressBar, 0, 3, 2, 1);
 
-    const startButton = new Button({label: 'Start Index'});
-    grid.attach(startButton, 1, 4, 1, 1);
+    const startButton = new Button({label: 'Start Index', halign: 2});
+    const startButtonBox = new Box({halign: 2});
+    startButtonBox.add(startButton);
+    grid.attach(startButtonBox, 1, 4, 1, 1);
 
 
     startButton.on('clicked', () => {
